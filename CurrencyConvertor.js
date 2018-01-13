c9d03c0fdf161be70431ef8c2c2ca23400067bf8
Mende <sm185402@ncr.com>
2018-01-13 23:04:48 +0530

    version 1.5

---
+++ CurrencyConvertor.js
@@ -0,0 +1,22 @@
+function convertor(i) {
+    var xhttp = new XMLHttpRequest();
+    var baseCurrency = document.getElementsByName("base_currency")[i];
+    var baseCurrencyText = baseCurrency.options[baseCurrency.selectedIndex].text;
+    var targetCurrency = document.getElementsByName("target_currency")[i];
+    var targetCurrencyText = targetCurrency.options[targetCurrency.selectedIndex].text;
+    
+    var url = "https://api.fixer.io/latest";
+    var params = "base="+baseCurrencyText+"&symbols="+targetCurrencyText;
+    xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        if(baseCurrencyText != targetCurrencyText) {
+            var obj = JSON.parse(this.responseText);
+            document.getElementsByName('convertor')[i].value = obj.rates[targetCurrencyText] * document.getElementsByName('convert')[i].value;
+        } else {
+            document.getElementsByName('convertor')[i].value = document.getElementsByName('convert')[i].value;
+        }
+    } 
+    }
+    xhttp.open("GET", url+"?"+params, true);
+    xhttp.send();
+}
